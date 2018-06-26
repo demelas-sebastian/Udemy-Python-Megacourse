37b0b27473874d8addc2637f66dd26bf386969bb
demelas-sebastian <demelas.sebastian@hotmail.com>
2018-06-26 14:42:05 -0300

    Built database GUI app

---
+++ 5/bookstore.py
@@ -0,0 +1,96 @@
+from tkinter import *
+import backend as bk
+
+def get_selected_row(event):
+    global selected_tuple
+    try:
+        index=box.curselection()[0]
+        selected_tuple=box.get(index)
+        e_author.delete(0,END)
+        e_title.delete(0,END)
+        e_year.delete(0,END)
+        e_isbn.delete(0,END)
+        e_title.insert(END,selected_tuple[1])
+        e_author.insert(END,selected_tuple[2])
+        e_year.insert(END,selected_tuple[3])
+        e_isbn.insert(END,selected_tuple[4])
+    except IndexError:
+        pass
+
+def view_command():
+    box.delete(0,END)
+    for row in bk.view():
+        box.insert(END,row)
+
+def search_command():
+    box.delete(0,END)
+    for row in bk.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+        box.insert(END,row)
+
+def add_command():
+    bk.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+    box.delete(0,END)
+    box.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
+
+def update_command():
+    bk.update(selected_tuple[0],e_title.get(),e_author.get(),e_year.get(),e_isbn.get())
+    box.delete(0,END)
+    for row in bk.view():
+        box.insert(END,row)
+
+def delete_command():
+    bk.delete(selected_tuple[0])
+    box.delete(0,END)
+    for row in bk.view():
+        box.insert(END,row)
+
+window=Tk()
+window.wm_title('BookStore')
+
+l_title=Label(window,text='Title')
+l_author=Label(window,text='Author')
+l_year=Label(window,text='Year')
+l_isbn=Label(window,text='ISBN')
+l_title.grid(row=0,column=0)
+l_author.grid(row=0,column=2)
+l_year.grid(row=1,column=0)
+l_isbn.grid(row=1,column=2)
+
+title_text=StringVar()
+author_text=StringVar()
+year_text=StringVar()
+isbn_text=StringVar()
+e_title=Entry(window,textvariable=title_text)
+e_author=Entry(window,textvariable=author_text)
+e_year=Entry(window,textvariable=year_text)
+e_isbn=Entry(window,textvariable=isbn_text)
+e_title.grid(row=0,column=1)
+e_author.grid(row=0,column=3)
+e_year.grid(row=1,column=1)
+e_isbn.grid(row=1,column=3)
+
+box=Listbox(window,height=6,width=35)
+box.grid(row=2,column=0,rowspan=6,columnspan=2)
+
+scroll_box=Scrollbar(window)
+scroll_box.grid(row=2,column=2,rowspan=6)
+
+box.configure(yscrollcommand=scroll_box.set)
+scroll_box.configure(command=box.yview)
+
+box.bind('<<ListboxSelect>>',get_selected_row)
+
+b_view=Button(window,text='View all',command=view_command,width=12)
+b_search=Button(window,text='Search entry',command=search_command,width=12)
+b_add=Button(window,text='Add entry',command=add_command,width=12)
+b_update=Button(window,text='Update',command=update_command,width=12)
+b_delete=Button(window,text='Delete',command=delete_command,width=12)
+b_close=Button(window,text='Close',command=lambda x=window:bk.close(x),width=12)
+b_view.grid(row=2,column=3)
+b_search.grid(row=3,column=3)
+b_add.grid(row=4,column=3)
+b_update.grid(row=5,column=3)
+b_delete.grid(row=6,column=3)
+b_close.grid(row=7,column=3)
+
+window.mainloop()
--- 5/script2.py
+++ 5/script2.py
@@ -1,21 +1,21 @@
 import psycopg2
 
 def create_table():
-    conn=sqlite3.connect('lite.db')
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='11235813' host='localhost' port='5432'")
     cur=conn.cursor()
     cur.execute('CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)')
     conn.commit()
     conn.close()
 
-def insert_data(item,quantity,price):
-    conn=sqlite3.connect('lite.db')
+def insert(item,quantity,price):
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='11235813' host='localhost' port='5432'")
     cur=conn.cursor()
-    cur.execute('INSERT INTO store VALUES (?,?,?)',(item,quantity,price))
+    cur.execute("INSERT INTO store VALUES (%s,%s,%s)",(item,quantity,price))
     conn.commit()
     conn.close()
 
 def view():
-    conn=sqlite3.connect('lite.db')
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='11235813' host='localhost' port='5432'")
     cur=conn.cursor()
     cur.execute('SELECT * FROM store')
     rows=cur.fetchall()
@@ -23,18 +23,20 @@ def view():
     return rows
 
 def delete(item):
-    conn=sqlite3.connect('lite.db')
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='11235813' host='localhost' port='5432'")
     cur=conn.cursor()
-    cur.execute('DELETE FROM store WHERE item=?',(item,))
+    cur.execute('DELETE FROM store WHERE item=%s',(item,))
     conn.commit()
     conn.close()
 
 def update(quantity,price,item):
-    conn=sqlite3.connect('lite.db')
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='11235813' host='localhost' port='5432'")
     cur=conn.cursor()
-    cur.execute('UPDATE store SET quantity=?, price=? WHERE item=?',(quantity,price,item))
+    cur.execute('UPDATE store SET quantity=%s, price=%s WHERE item=%s',(quantity,price,item))
     conn.commit()
     conn.close()
 
-update(11,6,'Water Glass')
+#update(11,6,'Water Glass')
 print(view())
+create_table()
+#insert('Orange',10,15)
